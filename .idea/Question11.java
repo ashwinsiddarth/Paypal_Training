c7f9ce025629de4b2c683284e423f3559c19a898
ashwinsiddarth <swethasiddhu03@gmail.com>
2020-12-28 10:16:13 +0530

    Question11 commit

---
+++ .idea/Question11.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Length of list : ");
+        n = sc.nextInt();
+        System.out.print("Input: [");
+        for(int i=1;i<=n;i++){
+            list.add(i);
+        }
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        System.out.println("]");
+        swap(list);
+
+    }
+    public static void swap(LinkedList list) {
+        if(list.size()==0){
+            System.out.println("Output = []");
+        }
+        else {
+            for (int i = 1; i < list.size(); i += 2) {
+                String temp = "" + list.get(i - 1);
+                int head = Integer.parseInt(temp);
+                temp = "" + list.get(i);
+                int next = Integer.parseInt(temp);
+                list.set(i, head);
+                list.set(i - 1, next);
+            }
+            System.out.println("Output : ");
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+}
